2c46eefb7aa3132e706f2e5033e4a344852cdc2b
Julio Luchiari <julio.luchiari@gmail.com>
2015-06-19 11:21:22 -0300

    start do projeto

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<title>Gerenciador</title>
+		<link rel="stylesheet" href="content/css/template.css">
+	</head>
+	
+	<body>
+		<div class="main">
+			<h1 class="titulo">Bem-vindo!</h1>
+		</div>	
+	</body>
+</html>
